8e40c896bf03b8fe3eb6840e537a0522c12fe656
DESKTOP-B07HMR9\81010 <810104157@qq.com>
2022-01-06 00:20:53 +0800

    ts

---
+++ TypeScript/类型/01_helloTs.ts
@@ -0,0 +1,18 @@
+console.log('hello ts')
+
+// let a: number;
+// a = 10;
+
+// let b: string;
+// b = '123';
+
+// 声明完变量可以直接赋值
+// let c: boolean = false;
+// 如果变量的声明和赋值是同时进行的，TS可以自动对变量进行类型检测
+// let c = true;
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+sum(a:123, b:456);
