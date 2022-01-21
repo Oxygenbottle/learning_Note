4d1f9c7d9339d95ebce607966627bdd29791ab3b
DESKTOP-7D8F7H9\86130 <linjinghui@zcunsoft.com>
2022-01-21 16:26:26 +0800

    上传代码

---
+++ 2022-01-16(es6实战)/es6 v2/my-project/src/components/myTree/tree.vue
@@ -0,0 +1,30 @@
+<script>
+import { refs } from "./refs";
+let count = 0;
+export default {
+  name: 'tree',
+  props: {
+    // 树形结构传参
+    options: {
+      type: Array,
+      // 对象或数组默认值必须从一个工厂函数获取
+      default: function () {
+        return [];
+      }
+    },
+    // 选中的节点
+    value: Array
+  },
+  data() {
+    let name = `v_tree_` + ++count;
+    return {
+      name
+    }
+  },
+  created() {
+    // 初始化
+    let name = this.name;
+    refs.init({ name }, this)
+  }
+}
+</script>
